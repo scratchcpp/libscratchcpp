b5883be0294fb37ea7564b34f4a32eba2e7af8f5
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-04-23 18:49:19 +0200

    StandardBlocks: Register all block sections

--- src/blocks/standardblocks.cpp
+++ src/blocks/standardblocks.cpp
@@ -4,6 +4,12 @@
 #include "controlblocks.h"
 #include "eventblocks.h"
 #include "motionblocks.h"
+#include "looksblocks.h"
+#include "soundblocks.h"
+#include "sensingblocks.h"
+#include "operatorblocks.h"
+#include "variableblocks.h"
+#include "listblocks.h"
 
 using namespace libscratchcpp;
 
@@ -25,6 +31,12 @@ bool StandardBlocks::includeByDefault() const
 void StandardBlocks::registerSections(Engine *engine)
 {
     engine->registerSection(std::make_shared<MotionBlocks>());
+    engine->registerSection(std::make_shared<LooksBlocks>());
+    engine->registerSection(std::make_shared<SoundBlocks>());
     engine->registerSection(std::make_shared<EventBlocks>());
     engine->registerSection(std::make_shared<ControlBlocks>());
+    engine->registerSection(std::make_shared<SensingBlocks>());
+    engine->registerSection(std::make_shared<OperatorBlocks>());
+    engine->registerSection(std::make_shared<VariableBlocks>());
+    engine->registerSection(std::make_shared<ListBlocks>());
 }
