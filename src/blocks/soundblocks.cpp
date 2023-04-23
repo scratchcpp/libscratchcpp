3e05b5da1d1f155bfdd1a110e9919518f760babc
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-04-23 21:03:54 +0200

    Remove iostream from block sections

--- src/blocks/soundblocks.cpp
+++ src/blocks/soundblocks.cpp
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "soundblocks.h"
-#include <iostream>
 
 using namespace libscratchcpp;
 
--- src/blocks/variableblocks.cpp
+++ src/blocks/variableblocks.cpp
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "variableblocks.h"
-#include <iostream>
 
 using namespace libscratchcpp;
 
