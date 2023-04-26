b83ec812d4c1ff0c1622e1bc840cd0954a3c292e
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-04-26 14:06:44 +0200

    IBlockSection: Add field value map

--- src/engine/iblocksection.h
+++ src/engine/iblocksection.h
@@ -60,6 +60,17 @@ class IBlockSection
             return -1;
         }
 
+        /*!
+         * Returns the ID of the given special field value.\n
+         * Used internally by Engine.
+         */
+        virtual int resolveFieldValue(const std::string &value) const final
+        {
+            if (m_fieldValues.count(value) == 1)
+                return m_fieldValues.at(value);
+            return -1;
+        }
+
     protected:
         /*!
          * Assigns a block implementation pointer to a block opcode.
@@ -78,10 +89,14 @@ class IBlockSection
         /*! Assigns a field ID to a field name. */
         virtual void addField(const std::string &name, int id) final { m_fields[name] = id; }
 
+        /*! Assigns a special field value ID to a value. */
+        virtual void addFieldValue(const std::string &value, int id) final { m_fieldValues[value] = id; }
+
     private:
         std::map<std::string, BlockImpl> m_blocks;
         std::map<std::string, int> m_inputs;
         std::map<std::string, int> m_fields;
+        std::map<std::string, int> m_fieldValues;
 };
 
 } // namespace libscratchcpp
