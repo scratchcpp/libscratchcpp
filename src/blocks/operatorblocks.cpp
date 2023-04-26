0261dc47f8130f54ad0f959874672953765a557f
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-04-26 18:58:18 +0200

    Add initial implementation of operator blocks
    * operator_add
    * operator_subtract
    * operator_multiply
    * operator_divide
    * operator_random
    * operator_lt
    * operator_equals
    * operator_gt
    * operator_and
    * operator_or
    * operator_not

--- src/blocks/operatorblocks.cpp
+++ src/blocks/operatorblocks.cpp
@@ -6,9 +6,85 @@ using namespace libscratchcpp;
 
 OperatorBlocks::OperatorBlocks()
 {
+    // Blocks
+    addBlock("operator_add", &OperatorBlocks::add);
+    addBlock("operator_subtract", &OperatorBlocks::subtract);
+    addBlock("operator_multiply", &OperatorBlocks::multiply);
+    addBlock("operator_divide", &OperatorBlocks::divide);
+    addBlock("operator_random", &OperatorBlocks::pickRandom);
+    addBlock("operator_lt", &OperatorBlocks::lessThan);
+    addBlock("operator_equals", &OperatorBlocks::equals);
+    addBlock("operator_gt", &OperatorBlocks::greaterThan);
+    addBlock("operator_and", &OperatorBlocks::andGate);
+    addBlock("operator_or", &OperatorBlocks::orGate);
+    addBlock("operator_not", &OperatorBlocks::notGate);
+
+    // Inputs
+    addInput("NUM1", NUM1);
+    addInput("NUM2", NUM2);
+    addInput("FROM", FROM);
+    addInput("TO", TO);
+    addInput("OPERAND1", OPERAND1);
+    addInput("OPERAND2", OPERAND2);
+    addInput("OPERAND", OPERAND);
 }
 
 std::string OperatorBlocks::name() const
 {
     return "Operators";
+}
+
+Value OperatorBlocks::add(const BlockArgs &args)
+{
+    return args.input(NUM1)->value() + args.input(NUM2)->value();
+}
+
+Value OperatorBlocks::subtract(const BlockArgs &args)
+{
+    return args.input(NUM1)->value() - args.input(NUM2)->value();
+}
+
+Value OperatorBlocks::multiply(const BlockArgs &args)
+{
+    return args.input(NUM1)->value() * args.input(NUM2)->value();
+}
+
+Value OperatorBlocks::divide(const BlockArgs &args)
+{
+    return args.input(NUM1)->value() / args.input(NUM2)->value();
+}
+
+Value OperatorBlocks::pickRandom(const BlockArgs &args)
+{
+    return randint(args.input(FROM)->value().toNumber(), args.input(TO)->value().toNumber());
+}
+
+Value OperatorBlocks::lessThan(const BlockArgs &args)
+{
+    return args.input(OPERAND1)->value() < args.input(OPERAND2)->value();
+}
+
+Value OperatorBlocks::equals(const BlockArgs &args)
+{
+    return args.input(OPERAND1)->value() == args.input(OPERAND2)->value();
+}
+
+Value OperatorBlocks::greaterThan(const BlockArgs &args)
+{
+    return args.input(OPERAND1)->value() > args.input(OPERAND2)->value();
+}
+
+Value OperatorBlocks::andGate(const BlockArgs &args)
+{
+    return args.input(OPERAND1)->value().toBool() && args.input(OPERAND2)->value().toBool();
+}
+
+Value OperatorBlocks::orGate(const BlockArgs &args)
+{
+    return args.input(OPERAND1)->value().toBool() || args.input(OPERAND2)->value().toBool();
+}
+
+Value OperatorBlocks::notGate(const BlockArgs &args)
+{
+    return !args.input(OPERAND)->value().toBool();
 }
--- src/blocks/operatorblocks.h
+++ src/blocks/operatorblocks.h
@@ -11,9 +11,32 @@ namespace libscratchcpp
 class LIBSCRATCHCPP_EXPORT OperatorBlocks : public IBlockSection
 {
     public:
+        enum Inputs
+        {
+            NUM1,
+            NUM2,
+            FROM,
+            TO,
+            OPERAND1,
+            OPERAND2,
+            OPERAND
+        };
+
         OperatorBlocks();
 
         std::string name() const override;
+
+        static Value add(const BlockArgs &args);
+        static Value subtract(const BlockArgs &args);
+        static Value multiply(const BlockArgs &args);
+        static Value divide(const BlockArgs &args);
+        static Value pickRandom(const BlockArgs &args);
+        static Value lessThan(const BlockArgs &args);
+        static Value equals(const BlockArgs &args);
+        static Value greaterThan(const BlockArgs &args);
+        static Value andGate(const BlockArgs &args);
+        static Value orGate(const BlockArgs &args);
+        static Value notGate(const BlockArgs &args);
 };
 
 } // namespace libscratchcpp
