5ef826d74da5237756e107d4864d74bc6a2b3608
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-08-25 23:02:02 +0200

    Add ZipReader class

---
+++ src/internal/zipreader.h
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <string>
+#include <zip.h>
+
+namespace libscratchcpp
+{
+
+class ZipReader
+{
+    public:
+        ZipReader(const std::string &fileName);
+        ZipReader(const char *fileName);
+        ZipReader(const ZipReader &) = delete;
+        ~ZipReader();
+
+        bool open();
+        void close();
+
+        size_t readFile(const std::string &fileName, void **buf);
+        std::string readFileToString(const std::string &fileName);
+
+    private:
+        std::string m_fileName;
+        struct zip_t *m_zip = nullptr;
+};
+
+} // namespace libscratchcpp
