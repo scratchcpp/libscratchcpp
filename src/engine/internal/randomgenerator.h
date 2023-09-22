25a49d92223e21297fcd6200d6e61e46709febaa
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-09-22 16:36:12 +0200

    Add RandomGenerator class

---
+++ src/engine/internal/randomgenerator.h
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <memory>
+#include <random>
+
+#include "irandomgenerator.h"
+
+namespace libscratchcpp
+{
+
+class RandomGenerator : public IRandomGenerator
+{
+    public:
+        RandomGenerator();
+        RandomGenerator(const RandomGenerator &) = delete;
+
+        static std::shared_ptr<RandomGenerator> instance();
+
+        long randint(long start, long end) const override;
+        double randintDouble(double start, double end) const override;
+
+    private:
+        static std::shared_ptr<RandomGenerator> m_instance;
+        std::random_device m_device;
+        std::unique_ptr<std::mt19937> m_generator;
+};
+
+} // namespace libscratchcpp
---
+++ test/mocks/randomgeneratormock.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <engine/internal/irandomgenerator.h>
+#include <gmock/gmock.h>
+
+using namespace libscratchcpp;
+
+class RandomGeneratorMock : public IRandomGenerator
+{
+    public:
+        MOCK_METHOD(long, randint, (long, long), (const, override));
+        MOCK_METHOD(double, randintDouble, (double, double), (const, override));
+};
