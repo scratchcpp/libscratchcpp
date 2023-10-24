aa643aa4391953c81b21a9793becea4f7841d555
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-10-24 23:10:40 +0200

    Add image format stub

---
+++ src/imageformats/stub/imageformatstub.cpp
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#include "imageformatstub.h"
+
+namespace libscratchcpp
+{
+
+ImageFormatStub::ImageFormatStub()
+{
+}
+
+void ImageFormatStub::setData(const char *)
+{
+}
+
+unsigned int ImageFormatStub::width() const
+{
+    return 0;
+}
+
+unsigned int ImageFormatStub::height() const
+{
+    return 0;
+}
+
+Rgb ImageFormatStub::colorAt(unsigned int, unsigned int, double) const
+{
+    return 0;
+}
+
+} // namespace libscratchcpp
