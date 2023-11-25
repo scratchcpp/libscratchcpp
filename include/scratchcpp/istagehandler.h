b2d541f7a3f23bd9165a7c9fe057718375de9959
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-11-25 15:47:00 +0100

    Document methods of target interfaces

--- include/scratchcpp/istagehandler.h
+++ include/scratchcpp/istagehandler.h
@@ -14,12 +14,19 @@ class LIBSCRATCHCPP_EXPORT IStageHandler
     public:
         virtual ~IStageHandler() { }
 
+        /*! Called when the interface is set on a stage. */
         virtual void init(Stage *stage) = 0;
 
+        /*! Called when the costume changes. */
         virtual void onCostumeChanged(Costume *costume) = 0;
 
+        /*! Called when the tempo changes. */
         virtual void onTempoChanged(int tempo) = 0;
+
+        /*! Called when the video state changes. */
         virtual void onVideoStateChanged(Stage::VideoState videoState) = 0;
+
+        /*! Called when the video transparency changes. */
         virtual void onVideoTransparencyChanged(int videoTransparency) = 0;
 };
 
