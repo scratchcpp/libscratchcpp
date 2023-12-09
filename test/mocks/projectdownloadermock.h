591c5827e7f1cd3fc692f56ef7dcab8a73146f10
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-12-09 13:39:41 +0100

    Add ProjectDownloaderMock

---
+++ test/mocks/projectdownloadermock.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <internal/iprojectdownloader.h>
+#include <gmock/gmock.h>
+
+using namespace libscratchcpp;
+
+class ProjectDownloaderMock : public IProjectDownloader
+{
+    public:
+        MOCK_METHOD(bool, downloadJson, (const std::string &), (override));
+        MOCK_METHOD(bool, downloadAssets, (const std::vector<std::string> &), (override));
+        MOCK_METHOD(void, cancel, (), (override));
+
+        MOCK_METHOD(void, setDownloadProgressCallback, (const std::function<void(unsigned int, unsigned int)> &), (override));
+
+        MOCK_METHOD(const std::string &, json, (), (const, override));
+        MOCK_METHOD(const std::vector<std::string> &, assets, (), (const, override));
+        MOCK_METHOD(unsigned int, downloadedAssetCount, (), (const, override));
+};
