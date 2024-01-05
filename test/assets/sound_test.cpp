f8a75f5c15c100df81fe86d6a5ca128c6382bb68
adazem009 <68537469+adazem009@users.noreply.github.com>
2024-01-05 14:21:32 +0100

    Fix data format in sound test

--- test/assets/sound_test.cpp
+++ test/assets/sound_test.cpp
@@ -6,18 +6,18 @@ using namespace libscratchcpp;
 
 TEST(SoundTest, Constructors)
 {
-    Sound sound("sound1", "a", "svg");
+    Sound sound("sound1", "a", "wav");
     ASSERT_EQ(sound.id(), "a");
     ASSERT_EQ(sound.name(), "sound1");
-    ASSERT_EQ(sound.dataFormat(), "svg");
-    ASSERT_EQ(sound.fileName(), "a.svg");
+    ASSERT_EQ(sound.dataFormat(), "wav");
+    ASSERT_EQ(sound.fileName(), "a.wav");
     ASSERT_EQ(sound.rate(), 0);
     ASSERT_EQ(sound.sampleCount(), 0);
 }
 
 TEST(SoundTest, Rate)
 {
-    Sound sound("sound1", "a", "svg");
+    Sound sound("sound1", "a", "wav");
 
     sound.setRate(2);
     ASSERT_EQ(sound.rate(), 2);
@@ -25,7 +25,7 @@ TEST(SoundTest, Rate)
 
 TEST(SoundTest, SampleCount)
 {
-    Sound sound("sound1", "a", "svg");
+    Sound sound("sound1", "a", "wav");
 
     sound.setSampleCount(10);
     ASSERT_EQ(sound.sampleCount(), 10);
