5bfc4f380a93160807525bb2949eca68e1337f11
Jessie YU <jy2728@nyu.edu>
2024-02-23 09:46:45 -0500

    Fix #493: Ignore leading/trailing spaces in url (#494)

--- test/network/projecturl_test.cpp
+++ test/network/projecturl_test.cpp
@@ -59,4 +59,23 @@ TEST(ProjectUrlTest, Parse)
         ASSERT_FALSE(url.isProjectUrl());
         ASSERT_TRUE(url.projectId().empty());
     }
+
+    {
+        ProjectUrl url("    https:/scratch.mit.edu/projects/886166351");
+        ASSERT_TRUE(url.isProjectUrl());
+        ASSERT_EQ(url.projectId(), "886166351");
+    }
+
+    {
+        ProjectUrl url("https:/scratch.mit.edu/projects/886166351    ");
+        ASSERT_TRUE(url.isProjectUrl());
+        ASSERT_EQ(url.projectId(), "886166351");
+    }
+
+    {
+        ProjectUrl url(" https:/scratch.mit.edu/projects/886166351  ");
+        ASSERT_TRUE(url.isProjectUrl());
+        ASSERT_EQ(url.projectId(), "886166351");
+    }
+    
 }
