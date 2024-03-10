a821878033c840305c8da1f2e2514d3250b2ecf0
adazem009 <68537469+adazem009@users.noreply.github.com>
2024-03-10 17:48:23 +0100

    Allow testing AudioOutput without audio support

--- test/audio/audiooutput_test.cpp
+++ test/audio/audiooutput_test.cpp
@@ -1,5 +1,10 @@
 #include <audio/audiooutput.h>
+
+#ifdef LIBSCRATCHCPP_AUDIO_SUPPORT
 #include <audio/internal/audioplayer.h>
+#else
+#include <audio/internal/audioplayerstub.h>
+#endif
 
 #include "../common.h"
 
@@ -10,5 +15,10 @@ TEST(AudioOutputTest, Create)
     auto factory = AudioOutput::instance();
     std::shared_ptr<IAudioPlayer> player = factory->createAudioPlayer();
     ASSERT_TRUE(player);
+
+#ifdef LIBSCRATCHCPP_AUDIO_SUPPORT
     ASSERT_TRUE(std::dynamic_pointer_cast<AudioPlayer>(player));
+#else
+    ASSERT_TRUE(std::dynamic_pointer_cast<AudioPlayerStub>(player));
+#endif
 }
