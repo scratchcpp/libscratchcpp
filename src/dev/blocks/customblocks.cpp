24b98b2dc08f09e246570bb48d5c8ebf064ade7c
adazem009 <68537469+adazem009@users.noreply.github.com>
2025-01-02 19:21:28 +0100

    Implement procedures_definition

--- src/dev/blocks/customblocks.cpp
+++ src/dev/blocks/customblocks.cpp
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
+#include <scratchcpp/iengine.h>
+#include <scratchcpp/dev/compiler.h>
+
 #include "customblocks.h"
 
 using namespace libscratchcpp;
@@ -16,4 +19,5 @@ std::string CustomBlocks::description() const
 
 void CustomBlocks::registerBlocks(IEngine *engine)
 {
+    engine->addCompileFunction(this, "procedures_definition", [](Compiler *) -> CompilerValue * { return nullptr; });
 }
--- test/dev/blocks/custom_blocks_test.cpp
+++ test/dev/blocks/custom_blocks_test.cpp
@@ -1,15 +1,40 @@
+#include <scratchcpp/project.h>
+#include <scratchcpp/dev/test/scriptbuilder.h>
+#include <scratchcpp/dev/compiler.h>
+#include <scratchcpp/sprite.h>
 #include <enginemock.h>
 
 #include "../common.h"
 #include "dev/blocks/customblocks.h"
 
 using namespace libscratchcpp;
+using namespace libscratchcpp::test;
 
 class CustomBlocksTest : public testing::Test
 {
     public:
-        void SetUp() override { m_extension = std::make_unique<CustomBlocks>(); }
+        void SetUp() override
+        {
+            m_extension = std::make_unique<CustomBlocks>();
+            m_engine = m_project.engine().get();
+            m_extension->registerBlocks(m_engine);
+        }
 
         std::unique_ptr<IExtension> m_extension;
+        Project m_project;
+        IEngine *m_engine = nullptr;
         EngineMock m_engineMock;
 };
+
+TEST_F(CustomBlocksTest, Definition)
+{
+    auto target = std::make_shared<Sprite>();
+    ScriptBuilder builder(m_extension.get(), m_engine, target);
+
+    builder.addBlock("procedures_definition");
+    auto block = builder.currentBlock();
+
+    Compiler compiler(&m_engineMock, target.get());
+    compiler.compile(block);
+    ASSERT_TRUE(compiler.unsupportedBlocks().empty());
+}
