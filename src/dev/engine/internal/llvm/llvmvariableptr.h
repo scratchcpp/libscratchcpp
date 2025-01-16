9f5f45ce0eb756d1ede89a718679f715993877ca
adazem009 <68537469+adazem009@users.noreply.github.com>
2025-01-16 23:22:19 +0100

    LLVMCodeBuilder: Store loop variable write instructions

--- src/dev/engine/internal/llvm/llvmvariableptr.h
+++ src/dev/engine/internal/llvm/llvmvariableptr.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <scratchcpp/dev/compiler.h>
+#include <unordered_map>
 
 namespace llvm
 {
@@ -14,6 +15,9 @@ class Value;
 namespace libscratchcpp
 {
 
+class LLVMLoopScope;
+class LLVMInstruction;
+
 struct LLVMVariablePtr
 {
         llvm::Value *stackPtr = nullptr;
@@ -21,6 +25,9 @@ struct LLVMVariablePtr
         Compiler::StaticType type = Compiler::StaticType::Unknown;
         bool onStack = false;
         bool changed = false;
+
+        // Used in build phase to check the type safety of variables in loops
+        std::unordered_map<std::shared_ptr<LLVMLoopScope>, std::vector<std::shared_ptr<LLVMInstruction>>> loopVariableWrites; // loop scope, write instructions
 };
 
 } // namespace libscratchcpp
