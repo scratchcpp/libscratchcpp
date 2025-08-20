8e42c38aa66baf9faf0b0ea353402558824d5ec2
adazem009 <68537469+adazem009@users.noreply.github.com>
2025-08-20 16:46:07 +0200

    LLVMCodeBuilder: Fix a typo in code analyzer comment

--- src/engine/internal/llvm/llvmcodebuilder.cpp
+++ src/engine/internal/llvm/llvmcodebuilder.cpp
@@ -55,7 +55,7 @@ std::shared_ptr<ExecutableCode> LLVMCodeBuilder::build()
     if (m_warp) {
 #ifdef ENABLE_CODE_ANALYZER
         // Analyze the script (type analysis, optimizations, etc.)
-        // NOTE: Do this only for non-warp scripts
+        // NOTE: Do this only for warp scripts
         m_codeAnalyzer.analyzeScript(m_instructions);
 #endif
     }
