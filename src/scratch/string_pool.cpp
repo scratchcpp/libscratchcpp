1d3c37df7a4c2070d039943de16304297c48fbb0
adazem009 <68537469+adazem009@users.noreply.github.com>
2025-08-29 14:31:55 +0200

    Add string_pool_clear_thread() function

--- src/scratch/string_pool.cpp
+++ src/scratch/string_pool.cpp
@@ -10,6 +10,8 @@
 #include <cassert>
 #include <algorithm>
 
+#include "string_pool_p.h"
+
 namespace libscratchcpp
 {
 
@@ -76,17 +78,25 @@ void string_pool_add_thread(Thread *thread)
 
 void string_pool_remove_thread(Thread *thread)
 {
-    // Free all strings in the thread (garbage collection)
-    assert(threadStrings.find(thread) != threadStrings.cend());
-    auto &strings = threadStrings[thread];
-
-    for (StringPtr *str : strings)
-        freeStrings.insert(std::pair<size_t, StringPtr *>(str->allocatedSize, str));
-
+    string_pool_clear_thread(thread);
     threadStrings.erase(thread);
 
     if (currentThread == thread)
         currentThread = nullptr;
+}
+
+void string_pool_clear_thread(Thread *thread)
+{
+    // Free all strings in the thread (garbage collection)
+    assert(threadStrings.find(thread) != threadStrings.cend());
+    auto &strings = threadStrings[thread];
+
+    for (StringPtr *str : strings) {
+        assert(std::find_if(freeStrings.begin(), freeStrings.end(), [str](const std::pair<size_t, StringPtr *> &p) { return p.second == str; }) == freeStrings.end());
+        freeStrings.insert(std::pair<size_t, StringPtr *>(str->allocatedSize, str));
+    }
+
+    strings.clear();
 }
 
 void string_pool_set_thread(Thread *thread)
--- src/scratch/string_pool_p.h
+++ src/scratch/string_pool_p.h
@@ -7,6 +7,7 @@ class Thread;
 
 void string_pool_add_thread(Thread *thread);
 void string_pool_remove_thread(Thread *thread);
+void string_pool_clear_thread(Thread *thread);
 void string_pool_set_thread(Thread *thread);
 
 } // namespace libscratchcpp
